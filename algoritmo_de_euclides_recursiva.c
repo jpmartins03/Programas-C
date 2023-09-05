3aa1c94dee54ba29f845b26785531ab319868e94
João Pedro Martins Montera <jpmartins1013@gmail.com>
2023-09-05 10:13:36 -0400

    Add files via upload

---
+++ algoritmo_de_euclides_recursiva.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int a, b;
+int main(int a, int b){
+	printf("Digite o valor de a: ");
+	scanf("%d", &a);
+	printf("Digite o valor de b: ");
+	scanf("%d", &b);
+
+	
+
+
+	return 0;
+}
